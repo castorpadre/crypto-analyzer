21d8392b123d25149c38e47c297564627e2f9e8d
castorpadre <emiliomisasg@gmail.com>
2024-07-07 13:15:56 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const cryptoDataDiv = document.getElementById("crypto-data");
+    cryptoDataDiv.innerHTML = "<p>Datos de criptomonedas se mostrarán aquí.</p>";
+});
